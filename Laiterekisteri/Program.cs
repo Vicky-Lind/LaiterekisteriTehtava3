9d79e5877bc4c69b25ce4f932c7bb4e9f1ed4fdc
Vicky-Lind <112470720+Vicky-Lind@users.noreply.github.com>
2024-02-08 13:59:05 +0200

    Add project files.

---
+++ Laiterekisteri/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laiterekisteri
+{
+    internal class Device
+    {
+        public string boughtDate = "01.01.2024";
+        public double boughtPrice = 10.00;
+        public int warrantyMonths = 10;
+
+        public Device()
+        {
+        }
+
+        public Device(string boughtDate, double boughtPrice, int warrantyMonths)
+        {
+            this.boughtDate = boughtDate;
+            this.boughtPrice = boughtPrice;
+            this.warrantyMonths = warrantyMonths;
+        }
+
+        public void SayOpinion()
+        {
+            Console.WriteLine("This is the main device category");
+            Console.WriteLine(boughtDate, boughtPrice, warrantyMonths);
+        }
+    }
+
+    internal class Computer : Device
+    {
+        public string size;
+
+        public Computer(string size)
+        {
+            this.size = size;
+        }
+
+        public new void SayOpinion()
+        {
+            Console.WriteLine("This is the computer category");
+            Console.WriteLine(size);
+        }
+    }
+
+    internal class Phone : Device
+    {
+    }
+
+    internal class Tablet : Device
+    {
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Device device = new Device("08.02.2024", 160.00, 12);
+            device.SayOpinion();
+
+            Computer computer = new Computer("16inch");
+
+            computer.boughtPrice = 180.60;
+
+            Console.ReadLine();
+        }
+    }
+}
